5fdba63c1a2499d5a92b5add6843bfef365c0645
Kevin Cartwright <kevincart@gmail.com>
2018-11-12 21:15:14 +1300

    starting my fork

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ var config = {
     height: 600,
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
@@ -32,3 +33,22 @@ function create ()
     });
 
 }
+
+// function preload () {
+//   this.load.image('sky', 'assets/sky.png');
+//   this.load.image('ground', 'assets/platform.png');
+//   this.load.image('star', 'assets/star.png');
+//   this.load.image('bomb', 'assets/bomb.png');
+//   this.load.spritesheet('dude',
+//     'assets/dude.png',
+//     { frameWidth: 32, frameHeight: 48}
+//   );
+// };
+//
+// function create () {
+//   this.add.image(400, 300, 'sky');
+// };
+//
+// function update () {
+//
+// };
